d109f0ff97393653849c88127213d7c59514f4bf
Caleb Anderson <buyananderson@gmail.com>
2021-12-01 07:51:41 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'categories/index'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
+  root to: 'categories#index'
+  resources :categories
+  resources :Transaction
 end
